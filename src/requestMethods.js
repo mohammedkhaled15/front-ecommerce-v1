d444d05fc45bad975ba57eb302865ada6ab3576b
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-04 14:42:22 +0200

    ..

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,6 +4,7 @@ const BASE_URL = "http://localhost:5000/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export const privateRequest = axios.create({
