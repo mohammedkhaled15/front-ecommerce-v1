509bb5631a0f06e7226459220a5154afe70378a6
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2025-03-17 01:15:10 +0200

    Create ShowRenderedHTML.js

---
+++ src/ShowRenderedHTML.js
@@ -0,0 +1,20 @@
+
+import { renderToStaticMarkup } from 'react-dom/server';
+import formatHTML from './formatHTML.js';
+
+export default function ShowRenderedHTML({children}) {
+  const markup = renderToStaticMarkup(
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+  return (
+    <>
+      <h1>Rendered HTML:</h1>
+      <pre>
+        {formatHTML(markup)}
+      </pre>
+    </>
+  );
+}
